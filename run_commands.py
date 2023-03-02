3bb020a566be91f81f90821d21d4fa0ceb39f53f
Mariyan Damyanov <marians1md@gmail.com>
2023-03-02 10:15:37 +0200

    Adding The option run different commands

--- run_commands.py
+++ run_commands.py
@@ -8,21 +8,55 @@ def run_command(directory, command):
     except Exception as e:
         print('Error while running command')
         print(f'Error: {e}')
+        print(f'Command: {command}')
+
+def read_file(file_name):
+    try:
+        with open(file_name) as file:
+            return file.readlines()
+    except FileNotFoundError as e:
+        print('Error while reading file')
+        print(f'Error: {e}')
+        print(f'File: {file_name}')
+
+def read_base_dir():
+    base_dir = ''
+
+    try:
+        with open('data/base_path.txt') as base_file:
+            base_dir = base_file.readline()
+    except FileNotFoundError:
+        base_dir = input('Enter base directory: ')
+
+    return base_dir
+
+def get_command_collection(**commands) -> list:
+    print(os.getcwd())
+
+    files = os.listdir(f'data')
+
+    command_file = 'default'
+
+    if 'default.txt' not in files:
+        command_file = input('Enter command file: ')
+
+    file = f'data/{command_file}.txt'
+
+    file_commands = read_file(file)
+        
+    commands = [tuple(command.split('|')) for command in file_commands]
+
+    return commands
 
 # Enter the base directory where the frontend and site folders are located
 # Else input the base directory when running the script
 # Example: C:\\Users\\User\Work\\project\\ui
-BASE_DIR = ''
+base_dir = read_base_dir()
 
-base_dir = BASE_DIR or input('Enter base directory: ')
-
-# Enter the commands to run
-# Example: (f'{base_dir}\\frontend', npm run dev)
-commands = [
-    (f'{base_dir}', 'echo "Hello World"'),
-]
+# Enter additional command collections in the data folder
+commands = get_command_collection()
 
 for directory, command in commands:
-    print(f'Running command: {command} in directory: {directory}')
-    run_command(directory, command)
-    time.sleep(0.5)
+    print(f'Running command: {command} in directory: {base_dir + directory}')
+    run_command(base_dir + directory, command)
+    time.sleep(0.2)
